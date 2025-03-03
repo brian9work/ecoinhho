c7135050654b339ea8ee0846cd550b0d6b0b825c
Brian <brian9work@gmail.com>
2025-03-02 22:26:45 -0600

    Correction of bugs

--- src/config/isServer.js
+++ src/config/isServer.js
@@ -1,4 +1,4 @@
-const origin = "http://localhost/ecoinhho/src/assets/"
-// const origin = "https://raw.githubusercontent.com/brian9work/ecoinhho/refs/heads/main/src/assets/"
+// const origin = "http://localhost/ecoinhho/src/assets/"
+const origin = "https://raw.githubusercontent.com/brian9work/ecoinhho/refs/heads/main/src/assets/"
 
 export default origin
